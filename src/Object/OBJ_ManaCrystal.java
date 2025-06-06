6f42cbe205c2200cdf57e175002a340a5d7d764b
tuyenle20 <lethingoctuyen10ta1@gmail.com>
2025-06-06 11:02:49 +0700

    Update OBJ_ManaCrystal.java

--- src/Object/OBJ_ManaCrystal.java
+++ src/Object/OBJ_ManaCrystal.java
@@ -10,7 +10,6 @@ public class OBJ_ManaCrystal extends Entity{
         super(gp);
         this.gp = gp;
         type = type_pickupOnly;
-        //value = 1;
         name = "Mana Crystak";
         down1 = setup("/objects/manacrystal_full",gp.tileSize,gp.tileSize);
         image = setup("/objects/manacrystal_full",gp.tileSize,gp.tileSize);
@@ -18,8 +17,6 @@ public class OBJ_ManaCrystal extends Entity{
     }
     public void use(Entity entity) {
         gp.playSE(2);
-        //gp.ui.addMessage("Mana +" + value);
-        //entity.mana += value;
     }
 }
 
